cb2406db43c67813a6fc65f2967400f62fff48fc
Aniket Maurya <theaniketmaurya@gmail.com>
2022-12-03 01:16:06 +0530

    fix

--- 03-pythonserve/app.py
+++ 03-pythonserve/app.py
@@ -2,6 +2,9 @@
 # !pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers -q
 # !pip install -U "clip@ git+https://github.com/openai/CLIP.git@main" -q
 
+import base64
+import io
+
 import lightning as L
 from lightning.app.components.serve import Image, PythonServer
 from pydantic import BaseModel
@@ -16,13 +19,18 @@ class SDServe(PythonServer):
         super().__init__(input_type=Prompt, output_type=Image, **kwargs)
         self.sd_variant = sd_variant
 
+    def serialize(image):
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
     def setup(self, *args, **kwargs) -> None:
         from stable_diffusion_inference import create_text2image
 
         self._model = create_text2image(self.sd_variant)
 
     def predict(self, request: Prompt):
-        return {"image": self._model(request.prompt)}
+        return {"image": self.serialize(self._model(request.prompt))}
 
 
 app = L.LightningApp(SDServe())
